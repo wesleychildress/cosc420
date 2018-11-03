e69fb98eba205f1f816c3caebabf8f7235fb2ff0
WesleyChildress <wesleychildress@icloud.com>
2018-11-03 11:46:41 -0400

    playing with randrange(1,1000)

--- randomArray.py
+++ randomArray.py
@@ -1,43 +1,23 @@
 #!/usr/bin/env python3.7
 
-"""
-mpirun -np 56 ./randomArray.py
-"""
+# randomArray.py
+# mpirun -np 56 ./randomArray.py
 
 from mpi4py import MPI
 import sys
 
-
+size = MPI.COMM_WORLD.Get_size()
+rank = MPI.COMM_WORLD.Get_rank()
+name = MPI.Get_processor_name()
 
 # seed the pseudorandom number generator
 from random import seed
 from random import random
 from random import randrange
-# seed random number generator
-# generate some random numbers
-print(random(), random(), random())
-# reset the seed
-# generate some random numbers
-print(random(), random(), random())
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-# seed the pseudorandom number generator
-from random import seed
-from random import random
-# seed random number generator
-# generate some random numbers
-print(random(), random(), random())
-# reset the seedx
-# generate some random numbers
-print(random(), random(), random())
 
-print(randrange(1,1000))
+# generate some random numbers
+# print(randrange(1,1000))
+
+sys.stdout.write(
+    "process %d of %d with random # %d on %s.\n"
+    % (rank, size, randrange(1,1000), name))
